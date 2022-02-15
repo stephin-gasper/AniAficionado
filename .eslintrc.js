13b5b4cb92fd940f282e5fa3f69a49537dbc6be9
Stephin Gasper <stephin.gasper21@gmail.com>
2022-02-15 22:39:24 +0530

    Add eslint-plugin-react-hooks package
    Update plugins in eslintrc file for same

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
   root: true,
   parser: '@babel/eslint-parser',
-  plugins: ['react', 'react-native', 'jsx-control-statements', 'jest'],
+  plugins: [
+    'react',
+    'react-native',
+    'react-hooks',
+    'jsx-control-statements',
+    'jest',
+  ],
   extends: ['airbnb', 'prettier', 'plugin:jsx-control-statements/recommended'],
 };
