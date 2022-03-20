8a132f6ff7721bd29b87ae301d6830f9e93fc732
Stephin Gasper <stephin.gasper21@gmail.com>
2022-03-20 23:08:08 +0530

    Add api for fetching latest episodes from animePahe
    - Create animePahe.js file
    - Add fetchLatestEpisodes function to make api call using axios to animePahe.com to get latest episodes list

---
+++ src/api/animePahe.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const animePaheBaseUrl = 'https://animepahe.com/api/';
+
+// eslint-disable-next-line import/prefer-default-export
+export const fetchLatestEpisodes = async ({page = 1} = {}) => {
+  try {
+    const {data} = await axios.get(animePaheBaseUrl, {
+      params: {
+        m: 'airing',
+        page,
+      },
+    });
+
+    return data.data;
+  } catch {
+    return null;
+  }
+};
