14bd9cb4386ec3753393bec85c1bc36e8ca60575
Stephin Gasper <stephin.gasper21@gmail.com>
2022-03-27 19:59:03 +0530

    Add react-native-scalable-image package
    - Add default mock for same in jest.setup

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,10 @@
+import React from 'react';
+import {Image as MockImage} from 'react-native';
 import '@testing-library/jest-native/extend-expect';
 import 'jest-styled-components/native';
 
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
+jest.mock('react-native-scalable-image', () => (props) => (
+  <MockImage {...props} />
+));
