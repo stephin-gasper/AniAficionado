8faaa9f1f076a9d7ccfb9da9d416ce28486f6cc3
Stephin Gasper <stephin.gasper21@gmail.com>
2022-09-11 18:14:45 +0530

    Update App.js to include Header component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import Home from 'screens/home';
+import Header from './components/header';
 
 const Stack = createNativeStackNavigator();
 
-const App = () => (
-  <NavigationContainer>
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={Home} />
-    </Stack.Navigator>
-  </NavigationContainer>
-);
+const App = () => {
+  const MemoizedHeader = useCallback(() => <Header />, []);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          header: MemoizedHeader,
+        }}>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default App;
