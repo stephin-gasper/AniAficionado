ebd8b0b08c5be76f755ce6c47297cc7dd97695fc
Stephin Gasper <stephin.gasper21@gmail.com>
2022-10-05 22:27:56 +0530

    Update colors in theme.js

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,5 +1,17 @@
+import RATING_SCALE_KEYS from './ratingScaleKeys';
+
 export default {
-  primaryColor: '#1e2226',
-  secondaryColor: '#d62839',
+  primaryColor: '#6a5498',
+  secondaryColor: '#9381b9',
+  tertiaryColor: '#352462',
   textColor: '#fffade',
+  ratingColors: {
+    [RATING_SCALE_KEYS.EXCELLENT]: '#019545',
+    [RATING_SCALE_KEYS.GREAT]: '#01ba38',
+    [RATING_SCALE_KEYS.GOOD]: '#8adc4c',
+    [RATING_SCALE_KEYS.OK]: '#eece35',
+    [RATING_SCALE_KEYS.BAD]: '#f25b2a',
+    [RATING_SCALE_KEYS.POOR]: '#e22026',
+    [RATING_SCALE_KEYS.UNRATED]: '#a1a0a1',
+  },
 };
