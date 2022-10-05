9f8c28cf8f289a1d633eb4a87b8fad867b1be522
Stephin Gasper <stephin.gasper21@gmail.com>
2022-10-06 00:18:51 +0530

    Update Header.js styling

--- src/components/header/Header.style.js
+++ src/components/header/Header.style.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components/native';
 
 export const HeaderWrapper = styled.View`
-  background-color: ${(props) => props.theme.primaryColor};
-  padding: 0 20px 10px;
+  background-color: ${props => props.theme.primaryColor};
+  padding: 0 10px 10px;
 `;
 
 export const LogoWrapper = styled.View`
